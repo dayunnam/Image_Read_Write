4e90d9452fb7a7e44dcbd3c3d65102f0efdbeb79
DAYUN NAM <36228548+dayunnam@users.noreply.github.com>
2021-01-25 11:01:04 +0900

    Update png2eps.py

--- png2eps.py
+++ png2eps.py
@@ -1,7 +1,12 @@
-
-
-
 from PIL import Image
+#import sys
+
+#print("Tyype (1)input-file and (2)output-file\n e.g., pic.png pic.eps\n"  )
+#in_file=sys.argv[1]
+#out_file=sys.argv[2]
+#print("Input File Path : " + in_file)
+#print("Output File Path : " + out_file)
+
 
 image_png = 'pic.png'
 im = Image.open(image_png)
